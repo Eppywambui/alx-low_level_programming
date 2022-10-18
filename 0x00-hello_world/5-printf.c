8a0d6e34815ca26dda6d9a7ef903e1ac955c924d
Eppywambui <epiphaniawambui@gmail.com>
2022-10-18 04:54:56 -0700

    5th commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry point of the program
+ *
+ * return:0 if no errors,return non zero if errors
+ */
+int main(void)
+{
+	print f("with proper grammar, but the outcome is a piece of art,\n")
+		return(0)
+}
