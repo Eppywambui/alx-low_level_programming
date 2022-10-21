d6561408c65586a4448e94e2b57e534c80026d87
Eppywambui <epiphaniawambui@gmail.com>
2022-10-21 04:03:05 -0700

    laening c 8th task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,18 @@
 #include	<stdio.h>
-/**	
-  *main-program	entry	point.	
-  *return:0	if	no	error,	non	zero	if	error.
-  */
+/**
+ *main-program	entry	point.
+ *Description;	program	that	prints	all	numbers	of	base16	in	lowercase.
+ *return:0	if	no	error,	non	zero	if	error.
+ */
 int	main(void)
 {
 	int	i;
-	char	hexvalues[]=
+	char	hexvalues[]	=
 		"0123456789abcdef";
 
-	for	(i=0;	i<16;	i++)
+	for	(i	=	0;	i	<	16;	i++)
 	{
-		putchar(hexvalues[i]);;
+		putchar(hexvalues[i]);
 	}
 	putchar('\n');
 	return	(0);
