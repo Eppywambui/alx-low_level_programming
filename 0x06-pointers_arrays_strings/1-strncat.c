688c84f7efd9f85b0a071828d0104f5582eada2a
Eppywambui <epiphaniawambui@gmail.com>
2022-10-27 06:28:24 -0700

    pointers annd arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
-/**_strncat	-	concates	twO	strings	using	at	most	n	bytes	from	src
+/**
+ *_strncat	-	concates	twO	strings	using	at	most	n	bytes	from	src
   *@dest:	string	that	acceepts	concatenated	string	from	src
   *@src:	string	to	be	concatenate
   *@n:	number	of	bytes	used	to	conctenate	src	to	dest
   *Return: pointer	to	string	dest
   */
-char	*-strncat(char	*dest,	char	*src,	int	n)
+char	*_strncat(char	*dest,	char	*src,	int	n)
 {
+
 	int	destLen,	srcLen;
 
-	for	(destLen	=	0;	dest[destLen]	!=	'\0';	destLen++);
-	
-	for	(srcLen	=	0;	src[srcLen]	!='\0'	&&	n	>	0;	srcLen++,	n--,	destLen++)
+	while
+	{
+		destLen	=	0;	dest[destLen]	!=	'\0';	destLen++;
+	}
+
+	for	(srcLen	=	0;	src[srcLen]	!=	'\0'	&&	n	>	0;	srcLen++,	n--,	destLen++)
 	{
 		dest[destLen]	=	src[srcLen];
 	}
