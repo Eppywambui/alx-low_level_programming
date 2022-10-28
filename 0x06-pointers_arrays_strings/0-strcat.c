c0fe461c142eeeed865d424acc515bd97c0c1ece
Eppywambui <epiphaniawambui@gmail.com>
2022-10-28 11:55:44 -0700

    loop and arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,21 +12,13 @@
 
 char	*_strcat(char	*dest,	char	*src)
 {
-	int	destLen	=	0;
-	int	srcLen;
+	int	index	=	0,	dest_len	=	0;
 
-	while	(dest[destLen]	!=	'\0')
-	{
-		destLen++;
-	}
+	while	(dest[index++])
+		dest_len++;
 
-	for	(srcLen	=	0;	src[srcLen]	!=	'\0';	srcLen++)
-	{
-		dest[destLen]	=	src[srcLen];
-		destLen++;
-	}
-
-	dest[destLen]	=	'\0';
+	for	(index	=	0;	src[index];	index++)
+		dest[dest_len++]	=	src[index];
 
 	return	(dest);
 }
