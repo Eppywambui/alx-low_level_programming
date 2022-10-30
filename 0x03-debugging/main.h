ff06908246c6e4b5d19e53879fbfad9832cf6625
Eppywambui <epiphaniawambui@gmail.com>
2022-10-30 12:23:48 -0700

    hello

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,10 +3,10 @@
 
 #include	<stdio.h>
 
-void	positive_or_negative(int	i);
-int	largest_number(int	a,	int	b,	int	c);
-int	convert_day(int,	int);
-void	print_remaining_days(int,	int,	int);
+void	positive_or_negative(int i);
+int	largest_number(int a, int b, int c);
+int	convert_day(int, int);
+void	print_remaining_days(int, int, int);
 
 #endif
 
